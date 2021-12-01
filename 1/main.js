bd6298b28dc2d551d91b9755ba1ff0fbe500c0ca
Bobby Holtzner <me@bobbyholtzner.com>
2021-12-01 06:33:51 -0500

    Part one

---
+++ 1/main.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+const file = fs.readFileSync("./dataset.txt");
+
+const lines = file.toString().split("\n");
+
+let current = null;
+let increaseCount = 0;
+let decreaseCount = 0;
+
+lines.forEach((l) => {
+  l = parseInt(l);
+  if (current === null) {
+    current = l;
+  } else if (current < l) {
+    increaseCount += 1;
+    current = l;
+  } else {
+    decreaseCount += 1;
+    current = l;
+  }
+});
+
+console.log(increaseCount);
+console.log(decreaseCount);
