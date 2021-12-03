22721c79bf0474c19df37f077291288a2ca53405
Bobby Holtzner <bobby.holtzner@heronsystems.com>
2021-12-03 11:51:21 -0500

    Part 2 and refactor

--- 3/main.js
+++ 3/main.js
@@ -1,6 +1,6 @@
 // Run this from the /3 directory because paths are relative
 const file = require("../loadFile");
-const lines = file.toString().split("\n");
+const dataset = file.toString().split("\n");
 
 const splitToColumns = (lines) => {
   const columns = {};
@@ -28,16 +28,79 @@ const getBitCounts = (arr) => {
   return bitCounts;
 };
 
-const getMostCommonBit = (bitCount) => {};
-const getLeastCommonBit = (bitCount) => {};
+const getCommonBit = (bitCount) => {
+  const arr = Object.values(bitCount);
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  const mostCommon = Object.keys(bitCount).find((key) => bitCount[key] === max);
+  const leastCommon = Object.keys(bitCount).find(
+    (key) => bitCount[key] === min
+  );
+  return {
+    most: mostCommon,
+    least: leastCommon,
+  };
+};
+
+const convertBinaryToDecimal = (binary) => {
+  return parseInt(binary, 2);
+};
 
 const part1 = (rows) => {
   const columns = splitToColumns(rows);
+  let gammaRate = "";
+  let epsilonRate = "";
   Object.keys(columns).forEach((key) => {
     const column = columns[key];
     const bitCounts = getBitCounts(column);
-    console.log(bitCounts);
+    const { most: gammaBit, least: epsilonBit } = getCommonBit(bitCounts);
+    gammaRate += gammaBit;
+    epsilonRate += epsilonBit;
   });
+  const gammaDecimal = convertBinaryToDecimal(gammaRate);
+  const epsilonDecimal = convertBinaryToDecimal(epsilonRate);
+
+  const powerConsumption = gammaDecimal * epsilonDecimal;
+  console.log(powerConsumption); // 1540244
 };
 
-part1(lines);
+const getRating = (type, values) => {
+  let param = null;
+  let equalValue = null;
+  if (type === "oxygen") {
+    param = "most";
+    equalValue = "1";
+  } else if (type === "co2") {
+    param = "least";
+    equalValue = "0";
+  }
+
+  const columnCount = values[0].length;
+
+  for (let i = 0; i < columnCount; i++) {
+    let columns = splitToColumns(values);
+    const counts = getBitCounts(columns[i]);
+    const bit = getCommonBit(counts);
+    if (bit.most === bit.least) {
+      bit[param] = equalValue;
+    }
+    values = values.filter((row) => row[i] === bit[param]);
+    if (values.length === 1) {
+      break;
+    }
+  }
+  return values;
+};
+
+const part2 = (values) => {
+  const oxygenRating = getRating("oxygen", values);
+  const co2Rating = getRating("co2", values);
+
+  const lifeSupportRating =
+    convertBinaryToDecimal(oxygenRating) * convertBinaryToDecimal(co2Rating);
+
+  console.log(lifeSupportRating); // 4203981
+};
+
+part1(dataset); // 1540244
+part2(dataset); // 4203981
