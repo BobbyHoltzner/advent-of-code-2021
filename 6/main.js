df5fbfa88f7c93ba5335702f07d7bb6392c57b82
Bobby Holtzner <bobby.holtzner@heronsystems.com>
2021-12-06 12:26:52 -0500

    Added code for part 2 and rewrote how we solve the problem

--- 6/main.js
+++ 6/main.js
@@ -1,33 +1,80 @@
-var __spreadArrays = (this && this.__spreadArrays) || function () {
-    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
-    for (var r = Array(s), k = 0, i = 0; i < il; i++)
-        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
-            r[k] = a[j];
-    return r;
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
 };
 var file = require("../loadFile");
 var dataset = file
     .toString()
     .split(",")
     .map(function (v) { return parseInt(v); });
-var part1 = function (days) {
-    var fish = __spreadArrays(dataset);
-    for (var i = 0; i < days; i++) {
-        fish.forEach(function (f, index, a) {
-            if (f === 0) {
-                a[index] = 6;
-                fish.push(8);
+/**
+ *  Creates a map/object which we will hold the counts
+ * @returns Object
+ */
+var generateMap = function () {
+    return {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0
+    };
+};
+var getCount = function (days) {
+    // Clone the dataset so we don't manipulate the original
+    var fish = __spreadArray([], dataset, true);
+    // Generate a clean map with all 0 counts
+    var map = generateMap();
+    // Load the initial counts in based on the first day/frame
+    fish.forEach(function (f) {
+        map[f] = map[f] + 1;
+    });
+    var _loop_1 = function (i) {
+        var mapCopy = __assign({}, map);
+        Object.keys(map).forEach(function (key) {
+            // Since the fish reproduces at 0 and the child starts at 8, we can copy the value at day 0 and make it the value at day 8
+            var nextKey = key === "8" ? "0" : (parseInt(key) + 1).toString();
+            if (nextKey === "0") {
+                mapCopy[key] = map[nextKey];
+                /// After the parent reproduces, it resets to 6. We need to be aware that others may be at 6 so we append the value to the existing
+                mapCopy["6"] = mapCopy[6] + map[nextKey];
             }
             else {
-                a[index] = a[index] - 1;
+                // Else we can grab the value from the higher index
+                mapCopy[key] = map[nextKey];
             }
         });
-        var memory = process.memoryUsage();
-        console.log(i, (memory.heapUsed / 1024 / 1024 / 1024).toFixed(4), "GB");
+        // Copy the map to the original so we aren't manipulating on each key
+        map = mapCopy;
+    };
+    // Loop through the days and update the counts
+    for (var i = 0; i < days; i++) {
+        _loop_1(i);
     }
-    return fish.length;
+    // Sum all of the values
+    return Object.values(map).reduce(function (a, b) { return a + b; });
 };
-// const fishCount = part1(80); // 5934
-// console.log(fishCount);
-var fishCountPart2 = part1(256);
+var fishCountPart1 = getCount(80); // 362346
+console.log(fishCountPart1);
+var fishCountPart2 = getCount(256); // 1639643057051
 console.log(fishCountPart2);
--- 6/main.ts
+++ 6/main.ts
@@ -5,25 +5,57 @@ const dataset: number[] = file
   .split(",")
   .map((v) => parseInt(v));
 
-const part1 = (days) => {
-  let fish = [...dataset];
-  for (let i = 0; i < days; i++) {
-    fish.forEach((f, index, a) => {
-      if (f === 0) {
-        a[index] = 6;
-        fish.push(8);
-      } else {
-        a[index] = a[index] - 1;
-      }
-    });
-    const memory = process.memoryUsage();
-    console.log(i, (memory.heapUsed / 1024 / 1024 / 1024).toFixed(4), "GB");
-  }
-  return fish.length;
+/**
+ *  Creates a map/object which we will hold the counts
+ * @returns Object
+ */
+const generateMap = () => {
+  return {
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+  };
 };
 
-// const fishCount = part1(80); // 5934
-// console.log(fishCount);
+const getCount = (days) => {
+  // Clone the dataset so we don't manipulate the original
+  let fish = [...dataset];
+  // Generate a clean map with all 0 counts
+  let map = generateMap();
+  // Load the initial counts in based on the first day/frame
+  fish.forEach((f) => {
+    map[f] = map[f] + 1;
+  });
+  // Loop through the days and update the counts
+  for (let i = 0; i < days; i++) {
+    const mapCopy = { ...map };
+    Object.keys(map).forEach((key) => {
+      // Since the fish reproduces at 0 and the child starts at 8, we can copy the value at day 0 and make it the value at day 8
+      const nextKey = key === "8" ? "0" : (parseInt(key) + 1).toString();
+      if (nextKey === "0") {
+        mapCopy[key] = map[nextKey];
+        /// After the parent reproduces, it resets to 6. We need to be aware that others may be at 6 so we append the value to the existing
+        mapCopy["6"] = mapCopy[6] + map[nextKey];
+      } else {
+        // Else we can grab the value from the higher index
+        mapCopy[key] = map[nextKey];
+      }
+    });
+    // Copy the map to the original so we aren't manipulating on each key
+    map = mapCopy;
+  }
+  // Sum all of the values
+  return Object.values(map).reduce((a, b) => a + b);
+};
 
-const fishCountPart2 = part1(256);
+const fishCountPart1 = getCount(80); // 362346
+console.log(fishCountPart1);
+
+const fishCountPart2 = getCount(256); // 1639643057051
 console.log(fishCountPart2);
