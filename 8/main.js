7cc7ce71c65165cd31612d31a14a26fbb05a7684
Bobby Holtzner <bobby.holtzner@heronsystems.com>
2021-12-08 21:57:30 -0500

    Added part 2 code

--- 8/main.js
+++ 8/main.js
@@ -1,18 +1,42 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var file = require("../loadFile");
 var dataset = file.toString().split("\n");
-var getCharactersPerSegment = function (segment) {
-    switch (segment) {
-        case "a":
-        case "d":
-        case "g":
-            return 4;
-        default:
-            return 2;
-    }
+var display = {
+    topLeft: "",
+    top: "",
+    topRight: "",
+    middle: "",
+    bottomRight: "",
+    bottom: "",
+    bottomLeft: ""
 };
-// Find the uniques and place them, we need to use the data before the pipe to determine the wires and reorganize the display per each
-// Loop through each line and create the display
-// Use that display
+var getDifference = function (a, b) {
+    return a.filter(function (x) { return !b.includes(x); });
+};
+function symmetricDifference(a1, a2) {
+    var result = [];
+    for (var i = 0; i < a1.length; i++) {
+        if (a2.indexOf(a1[i]) === -1) {
+            result.push(a1[i]);
+        }
+    }
+    for (i = 0; i < a2.length; i++) {
+        if (a1.indexOf(a2[i]) === -1) {
+            result.push(a2[i]);
+        }
+    }
+    return result;
+}
 var displayPart1 = {
     0: ["a", "b", "c", "e", "f", "g"],
     1: ["c", "f"],
@@ -25,17 +49,27 @@ var displayPart1 = {
     8: ["a", "b", "c", "d", "e", "f", "g"],
     9: ["a", "b", "c", "d", "f", "g"]
 };
-var displayPart2 = {
-    0: ["e", "d", "a", "b", "c", "g"],
-    1: ["a", "b"],
-    2: ["d", "a", "f", "g", "c"],
-    3: ["d", "a", "f", "b", "c"],
-    4: ["e", "f", "a", "b"],
-    5: ["d", "e", "f", "b", "c"],
-    6: ["d", "e", "f", "g", "b", "c"],
-    7: ["d", "a", "b"],
-    8: ["a", "b", "c", "d", "e", "f", "g"],
-    9: ["d", "e", "a", "f", "b", "c"]
+var createDisplay = function (v) {
+    return {
+        0: [v.top, v.topRight, v.bottomRight, v.bottom, v.bottomLeft, v.topLeft],
+        1: [v.topRight, v.bottomRight],
+        2: [v.top, v.topRight, v.middle, v.bottomLeft, v.bottom],
+        3: [v.top, v.topRight, v.middle, v.bottomRight, v.bottom],
+        4: [v.topLeft, v.middle, v.topRight, v.bottomRight],
+        5: [v.top, v.topLeft, v.middle, v.bottomRight, v.bottom],
+        6: [v.top, v.topLeft, v.bottomLeft, v.middle, v.bottom, v.bottomRight],
+        7: [v.top, v.topRight, v.bottomRight],
+        8: [
+            v.top,
+            v.topRight,
+            v.bottomRight,
+            v.middle,
+            v.bottom,
+            v.bottomLeft,
+            v.topLeft,
+        ],
+        9: [v.top, v.topRight, v.bottomRight, v.middle, v.bottom, v.topLeft]
+    };
 };
 var getSegmentCounts = function (display) {
     var map = {};
@@ -44,8 +78,59 @@ var getSegmentCounts = function (display) {
     });
     return map;
 };
-var getDisplay = function (line) {
-    console.log(line);
+var getTopRight = function (digit1, digit6) {
+    var split1 = digit1.split("");
+    return split1.find(function (v) {
+        return !digit6.includes(v);
+    });
+};
+var getDisplay = function (lineValues) {
+    var available = "abcdefg";
+    var d = __assign({}, display);
+    var two = lineValues.find(function (v) { return v.length === 2; }).split("");
+    var three = lineValues.find(function (v) { return v.length === 3; }).split("");
+    var four = lineValues.find(function (v) { return v.length === 4; });
+    var five = lineValues.filter(function (v) { return v.length === 5; });
+    var six = lineValues.filter(function (v) { return v.length === 6; });
+    var seven = lineValues.find(function (v) { return v.length === 7; });
+    var digit8 = seven;
+    // We can find 6 because it's the only one of the 6 segment digits that uses both of the segments from digit 1
+    var digit1 = lineValues.find(function (v) { return v.length === 2; });
+    var digit6 = six.find(function (v) {
+        return (v.includes(two[0]) || v.includes(two[1])) &&
+            (!v.includes(two[0]) || !v.includes(two[1]));
+    });
+    // We can find 3 because it's the only one of the 5 segment digits that uses both of the segments from digit 1
+    var digit3 = five.find(function (v) { return v.includes(two[0]) && v.includes(two[1]); });
+    // Now that we have the segments for 6, we can figure out which one of digit one is not included and can assume it's the top right
+    d.topRight = getTopRight(digit1, digit6);
+    available = available.replace(d.topRight, "");
+    d.bottomRight = digit1.replace(d.topRight, "");
+    available = available.replace(d.bottomRight, "");
+    // We know that two and five both include one of the segments from one but not both
+    var twoAndFive = five.filter(function (v) {
+        return (v.includes(two[0]) || v.includes(two[1])) &&
+            (!v.includes(two[0]) || !v.includes(two[1]));
+    });
+    // Since we figured out which segment was top right, we can determine which 5 segment is digit 2 and which is digit 5
+    var digit5 = twoAndFive.find(function (v) { return !v.includes(d.topRight); });
+    // Since we now have digit 5, we can get the difference between 5 and 6 and assume it's bottom left
+    d.bottomLeft = symmetricDifference(digit5, digit6)[0];
+    available = available.replace(d.bottomLeft, "");
+    // Top is the difference between the two segment (digit 1) and three segment (digit 7)
+    d.top = symmetricDifference(two, three).toString();
+    available = available.replace(d.top, "");
+    // We can get 0 because we know that it has both topRight and bottomLeft
+    var digit0 = six.find(function (v) { return v.includes(d.topRight) && v.includes(d.bottomLeft); });
+    d.middle = symmetricDifference(digit0, digit8)[0];
+    available = available.replace(d.middle, "");
+    d.topLeft = four
+        .replace(d.middle, "")
+        .replace(d.topRight, "")
+        .replace(d.bottomRight, "");
+    available = available.replace(d.topLeft, "");
+    d.bottom = available;
+    return createDisplay(d);
 };
 var getKeyByValue = function (object, value) {
     return Object.keys(object).find(function (key) { return object[key] === value; });
@@ -74,12 +159,9 @@ var getValueForString = function (display, str) {
         .filter(function (key) { return display[key].length === length; })
         .map(function (key) { return ({ key: key, value: display[key] }); });
     var stringSplit = str.split("");
-    // console.log(str);
-    var found = possibleValuesBasedOnLength.find(function (p) {
+    return possibleValuesBasedOnLength.find(function (p) {
         return stringSplit.every(function (v) { return p.value.includes(v); });
     });
-    // console.log("Found: ", found);
-    return found;
 };
 var partOne = function () {
     var segmentCounts = getSegmentCounts(displayPart1);
@@ -98,21 +180,20 @@ var partOne = function () {
     return count;
 };
 var partTwo = function () {
-    var segmentCounts = getSegmentCounts(displayPart2);
+    var segmentCounts = getSegmentCounts(displayPart1);
     var uniques = getUniques(segmentCounts);
     var instructions = [];
     var displayInstructions = dataset
         .map(function (d) { return d.split("|")[0].trim(); })
         .map(function (v) { return v.split(" "); });
-    console.log(displayInstructions);
+    // console.log(displayInstructions);
     dataset
         .map(function (d) { return d.split("|")[1].trim(); })
         .map(function (v) { return v.split(" "); })
         .forEach(function (v) { return instructions.push(v); });
     var values = {};
     instructions.forEach(function (instruction, index) {
-        // Create a display here...
-        // const display = getDisplay(instruction);
+        var display = getDisplay(displayInstructions[index]);
         instruction.forEach(function (single) {
             if (!values[index]) {
                 values[index] = [];
@@ -122,7 +203,7 @@ var partTwo = function () {
             }
             else {
                 // Pass the new display in, do we need to get segment counts for each?
-                var value = getValueForString(displayPart2, single);
+                var value = getValueForString(display, single);
                 if (value) {
                     values[index].push(value.key);
                 }
@@ -130,7 +211,6 @@ var partTwo = function () {
         });
     });
     var total = 0;
-    // console.log(values["3"]);
     Object.keys(values).forEach(function (v) {
         var value = values[v].toString().replace(/,/g, "");
         total += parseInt(value);
@@ -138,10 +218,6 @@ var partTwo = function () {
     return total;
 };
 var count = partOne();
-console.log(count);
+console.log(count); //255
 var total = partTwo();
-// console.log(total);
-var checker = function (arr, target) { return target.every(function (v) { return arr.includes(v); }); };
-var test = ["d", "e", "a", "f", "b", "c"];
-var val = ["e", "f", "a", "b", "c", "d"];
-// console.log(checker(test, val));
+console.log(total); // 982158
--- 8/main.ts
+++ 8/main.ts
@@ -2,20 +2,34 @@ const file = require("../loadFile");
 
 const dataset: string[] = file.toString().split("\n");
 
-const getCharactersPerSegment = (segment: string) => {
-  switch (segment) {
-    case "a":
-    case "d":
-    case "g":
-      return 4;
-    default:
-      return 2;
-  }
+const display = {
+  topLeft: "",
+  top: "",
+  topRight: "",
+  middle: "",
+  bottomRight: "",
+  bottom: "",
+  bottomLeft: "",
 };
 
-// Find the uniques and place them, we need to use the data before the pipe to determine the wires and reorganize the display per each
-// Loop through each line and create the display
-// Use that display
+const getDifference = (a, b) => {
+  return a.filter((x) => !b.includes(x));
+};
+
+function symmetricDifference(a1, a2) {
+  var result = [];
+  for (var i = 0; i < a1.length; i++) {
+    if (a2.indexOf(a1[i]) === -1) {
+      result.push(a1[i]);
+    }
+  }
+  for (i = 0; i < a2.length; i++) {
+    if (a1.indexOf(a2[i]) === -1) {
+      result.push(a2[i]);
+    }
+  }
+  return result;
+}
 
 const displayPart1 = {
   0: ["a", "b", "c", "e", "f", "g"],
@@ -30,17 +44,27 @@ const displayPart1 = {
   9: ["a", "b", "c", "d", "f", "g"],
 };
 
-const displayPart2 = {
-  0: ["e", "d", "a", "b", "c", "g"],
-  1: ["a", "b"],
-  2: ["d", "a", "f", "g", "c"],
-  3: ["d", "a", "f", "b", "c"],
-  4: ["e", "f", "a", "b"],
-  5: ["d", "e", "f", "b", "c"],
-  6: ["d", "e", "f", "g", "b", "c"],
-  7: ["d", "a", "b"],
-  8: ["a", "b", "c", "d", "e", "f", "g"],
-  9: ["d", "e", "a", "f", "b", "c"],
+const createDisplay = (v) => {
+  return {
+    0: [v.top, v.topRight, v.bottomRight, v.bottom, v.bottomLeft, v.topLeft],
+    1: [v.topRight, v.bottomRight],
+    2: [v.top, v.topRight, v.middle, v.bottomLeft, v.bottom],
+    3: [v.top, v.topRight, v.middle, v.bottomRight, v.bottom],
+    4: [v.topLeft, v.middle, v.topRight, v.bottomRight],
+    5: [v.top, v.topLeft, v.middle, v.bottomRight, v.bottom],
+    6: [v.top, v.topLeft, v.bottomLeft, v.middle, v.bottom, v.bottomRight],
+    7: [v.top, v.topRight, v.bottomRight],
+    8: [
+      v.top,
+      v.topRight,
+      v.bottomRight,
+      v.middle,
+      v.bottom,
+      v.bottomLeft,
+      v.topLeft,
+    ],
+    9: [v.top, v.topRight, v.bottomRight, v.middle, v.bottom, v.topLeft],
+  };
 };
 
 const getSegmentCounts = (display) => {
@@ -51,8 +75,69 @@ const getSegmentCounts = (display) => {
   return map;
 };
 
-const getDisplay = (line: string) => {
-  console.log(line);
+const getTopRight = (digit1, digit6) => {
+  const split1 = digit1.split("");
+  return split1.find((v) => {
+    return !digit6.includes(v);
+  });
+};
+
+const getDisplay = (lineValues: string[]) => {
+  let available = "abcdefg";
+  const d = { ...display };
+  const two = lineValues.find((v) => v.length === 2).split("");
+  const three = lineValues.find((v) => v.length === 3).split("");
+  const four = lineValues.find((v) => v.length === 4);
+  const five = lineValues.filter((v) => v.length === 5);
+  const six = lineValues.filter((v) => v.length === 6);
+  const seven = lineValues.find((v) => v.length === 7);
+  const digit8 = seven;
+  // We can find 6 because it's the only one of the 6 segment digits that uses both of the segments from digit 1
+  const digit1 = lineValues.find((v) => v.length === 2);
+  const digit6 = six.find(
+    (v) =>
+      (v.includes(two[0]) || v.includes(two[1])) &&
+      (!v.includes(two[0]) || !v.includes(two[1]))
+  );
+  // We can find 3 because it's the only one of the 5 segment digits that uses both of the segments from digit 1
+  const digit3 = five.find((v) => v.includes(two[0]) && v.includes(two[1]));
+  // Now that we have the segments for 6, we can figure out which one of digit one is not included and can assume it's the top right
+  d.topRight = getTopRight(digit1, digit6);
+  available = available.replace(d.topRight, "");
+  d.bottomRight = digit1.replace(d.topRight, "");
+  available = available.replace(d.bottomRight, "");
+
+  // We know that two and five both include one of the segments from one but not both
+  const twoAndFive = five.filter(
+    (v) =>
+      (v.includes(two[0]) || v.includes(two[1])) &&
+      (!v.includes(two[0]) || !v.includes(two[1]))
+  );
+  // Since we figured out which segment was top right, we can determine which 5 segment is digit 2 and which is digit 5
+  const digit5 = twoAndFive.find((v) => !v.includes(d.topRight));
+  // Since we now have digit 5, we can get the difference between 5 and 6 and assume it's bottom left
+  d.bottomLeft = symmetricDifference(digit5, digit6)[0];
+  available = available.replace(d.bottomLeft, "");
+
+  // Top is the difference between the two segment (digit 1) and three segment (digit 7)
+  d.top = symmetricDifference(two, three).toString();
+  available = available.replace(d.top, "");
+
+  // We can get 0 because we know that it has both topRight and bottomLeft
+  const digit0 = six.find(
+    (v) => v.includes(d.topRight) && v.includes(d.bottomLeft)
+  );
+  d.middle = symmetricDifference(digit0, digit8)[0];
+  available = available.replace(d.middle, "");
+
+  d.topLeft = four
+    .replace(d.middle, "")
+    .replace(d.topRight, "")
+    .replace(d.bottomRight, "");
+  available = available.replace(d.topLeft, "");
+
+  d.bottom = available;
+  return createDisplay(d);
 };
 
 const getKeyByValue = (object, value) => {
@@ -84,12 +169,9 @@ const getValueForString = (display: Object, str: string) => {
     .filter((key) => display[key].length === length)
     .map((key) => ({ key, value: display[key] }));
   const stringSplit = str.split("");
-  // console.log(str);
-  const found = possibleValuesBasedOnLength.find((p) => {
+  return possibleValuesBasedOnLength.find((p) => {
     return stringSplit.every((v) => p.value.includes(v));
   });
-  // console.log("Found: ", found);
-  return found;
 };
 
 const partOne = () => {
@@ -111,14 +193,14 @@ const partOne = () => {
 };
 
 const partTwo = () => {
-  const segmentCounts = getSegmentCounts(displayPart2);
+  const segmentCounts = getSegmentCounts(displayPart1);
   let uniques = getUniques(segmentCounts);
   const instructions = [];
   const displayInstructions = dataset
     .map((d: string) => d.split("|")[0].trim())
     .map((v) => v.split(" "));
 
-  console.log(displayInstructions);
+  // console.log(displayInstructions);
   dataset
     .map((d: string) => d.split("|")[1].trim())
     .map((v) => v.split(" "))
@@ -126,8 +208,7 @@ const partTwo = () => {
 
   const values = {};
   instructions.forEach((instruction, index) => {
-    // Create a display here...
-    // const display = getDisplay(instruction);
+    const display = getDisplay(displayInstructions[index]);
     instruction.forEach((single) => {
       if (!values[index]) {
         values[index] = [];
@@ -136,7 +217,7 @@ const partTwo = () => {
         values[index].push(getKeyByValue(segmentCounts, single.length));
       } else {
         // Pass the new display in, do we need to get segment counts for each?
-        const value = getValueForString(displayPart2, single);
+        const value = getValueForString(display, single);
         if (value) {
           values[index].push(value.key);
         }
@@ -144,7 +225,6 @@ const partTwo = () => {
     });
   });
   let total = 0;
-  // console.log(values["3"]);
   Object.keys(values).forEach((v) => {
     const value = values[v].toString().replace(/,/g, "");
     total += parseInt(value);
@@ -153,13 +233,7 @@ const partTwo = () => {
 };
 
 const count = partOne();
-console.log(count);
+console.log(count); //255
 
 const total = partTwo();
-// console.log(total);
-
-let checker = (arr, target) => target.every((v) => arr.includes(v));
-const test = ["d", "e", "a", "f", "b", "c"];
-const val = ["e", "f", "a", "b", "c", "d"];
-
-// console.log(checker(test, val));
+console.log(total); // 982158
